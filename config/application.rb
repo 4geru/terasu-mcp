7ee5f0c46ef8bf75baf2b653a70eb4f30ce13fae
4geru <westhouse51@gmail.com>
2026-05-10 18:57:39 +0900

    use doorkeeper and devise

--- config/application.rb
+++ config/application.rb
@@ -40,5 +40,10 @@ module ProjectRailsRemoteMcp
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Doorkeeper の認可フローでセッション・Flash が必要なため追加
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore, key: "_terasu_session"
+    config.middleware.use ActionDispatch::Flash
   end
 end
